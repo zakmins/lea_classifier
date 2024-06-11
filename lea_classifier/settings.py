acf350f9b73e21120d8ce0912d4bba6d8e83a4b4
Zakaria <89365259+zakmins@users.noreply.github.com>
2024-06-11 18:13:30 +0100

    pushh

--- lea_classifier/settings.py
+++ lea_classifier/settings.py
@@ -126,6 +126,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories that contain static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# Media files (Uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
